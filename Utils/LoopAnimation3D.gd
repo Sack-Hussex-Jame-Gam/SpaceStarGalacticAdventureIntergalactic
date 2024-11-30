2a2fd2a6b46436bf39c8ecd6d0827769c8b09544
Jae <71599416+Jheesbrough@users.noreply.github.com>
2024-11-30 21:56:40 +0000

    Massive progress on level2

---
+++ Utils/LoopAnimation3D.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@export var frames = 10
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+#	Change the frame 20 times a second
+	# Get quanity of frames from the animation
+	if frame < frames -1:
+		frame += 1
+	else:
+		frame = 0
+	pass
+	
+	
+	
